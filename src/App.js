9cecbdbecda0f4f93a1dac30c9ad961a3834c38a
Mark Aiken <gwmaiken@att.net>
2020-06-01 18:15:40 -0700

    Favorite Vs All album state updating

--- src/App.js
+++ src/App.js
@@ -4,22 +4,66 @@ import './App.css';
 import fetchTopAlbums from "./top-albums";
 import { useState, useEffect } from 'react';
 import _ from "lodash";
+import { configureStore, createSlice } from '@reduxjs/toolkit'
+import {Provider, useSelector} from 'react-redux'
+import {combineReducers} from 'redux'
+
+
+const favoriteOrAllSlice = createSlice({
+  name: 'favoriteOrAll',
+  initialState: "All",
+  reducers: {
+    favorite: state => "Favorite",
+    all: state =>  "All"
+  }
+})
+
+const store = configureStore({
+  reducer: combineReducers({
+    rootFilter: favoriteOrAllSlice.reducer
+  })
+})
 
 function AlbumCard({album}){
-  console.log(album)
+  // console.log(album)
   let width = "250px"
   return(
-    <div className="bg-black-10 pa3 ma2 br3" style={{width}}>
-      <img src={_.last(album?.["im:image"])?.label}/>
-      <div className="b">
-        {album?.["im:artist"]?.label}
+      <div className="bg-black-10 pa3 ma2 br3" style={{width}}>
+        <img src={_.last(album?.["im:image"])?.label}/>
+        <div className="b">
+          {album?.["im:artist"]?.label}
+        </div>
+        <p>
+          {album?.["im:name"]?.label}
+        </p>
       </div>
-      <p>
-        {album?.["im:name"]?.label}
-      </p>
-    </div>
   )
 }
+
+
+function TopNav() {
+  const { actions, reducer } = favoriteOrAllSlice
+  const { favorite, all} = actions
+  const albumFilter = useSelector(x => x?.rootFilter)
+  console.log(albumFilter)
+  return(
+      <header className="bg-green flex">
+        <div className="pa3 mr-auto">
+          <b>Top Albums</b>
+        </div>
+        <div className="pa3 ml-auto">
+          <a className={albumFilter === "All" ? "b" : "p" }
+             onClick={() => store.dispatch(all())}>
+            Top Albums
+          </a>
+          <span> | </span>
+          <a className={albumFilter === "Favorite" ? "b" : "p" }
+             onClick={() => store.dispatch(favorite())}>
+            Favorite Albums
+          </a>
+        </div>
+      </header>
+)}
 
 function App() {
   const [albums, setAlbums] = useState({})
@@ -30,21 +74,16 @@ function App() {
 
   let entry = albums?.feed?.entry;
   return (
+    <Provider store={store}>
       <div className="App">
-        <header className="bg-green flex">
-          <div className="pa3 mr-auto">
-            <b>Top Albums</b>
-          </div>
-          <div className="pa3 ml-auto">
-            Top Albums | Favorite Albums
-          </div>
-        </header>
+        <TopNav/>
         <div className="flex flex-wrap justify-around">
           {_.map(entry, x =>
             <AlbumCard album={x} key={x?.attributes?.["im:id"]}/>
           )}
         </div>
       </div>
+    </Provider>
   );
 }
 
